27a005ce61fd563789d3750e354b92eac562e516
alex rodríguez santibáñez <alex@alexrds.com>
2017-12-19 03:10:10 +0100

    Ajuste Menú Handheld
    Bye 1er botón

--- functions.php
+++ functions.php
@@ -136,23 +136,7 @@ function jk_remove_handheld_footer_links( $links ) {
 	return $links;
 }
 
-add_filter( 'storefront_handheld_footer_bar_links', 'jk_add_home_link' );
-function jk_add_home_link( $links ) {
-	$new_links = array(
-		'home' => array(
-			'priority' => 10,
-			'callback' => 'jk_home_link',
-		),
-	);
 
-	$links = array_merge( $new_links, $links );
-
-	return $links;
-}
-
-function jk_home_link() {
-	echo '<a href="' . esc_url( home_url( '/checkout' ) ) . '">' . __( 'Home' ) . '</a>';
-}
 
 /*** Esconde el contador de productos **/
 add_filter( 'woocommerce_subcategory_count_html', '__return_null' );
